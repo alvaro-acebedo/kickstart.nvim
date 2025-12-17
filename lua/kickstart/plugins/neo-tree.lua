79563b08827fef51fcac7dd7c4ca09a12c274d01
Alvaro Acebedo <metakrtx@gmail.com>
2025-12-17 15:29:37 +0100

    fix: change reveal and close window keymaps for neo-tree plugin

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'º', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['º'] = 'close_window',
         },
       },
     },
