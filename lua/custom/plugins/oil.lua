b99c69774ace46d29650182d10d97218962f04d5
Alvaro Acebedo <metakrtx@gmail.com>
2025-12-17 16:02:17 +0100

    fix: corrections in oil (stevearc/oil) plugin configuration

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,9 +2,9 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
+  opts = {},
   -- dependencies = { { 'echasnovski/mini-icons', opts = {} } },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   -- Open parent directory in current window
